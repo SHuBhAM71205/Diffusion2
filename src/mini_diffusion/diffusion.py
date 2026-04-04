dd36101553de477e7ca99da65027d0033c69dd78
SHuBhAM71205 <sp710812gs@gmail.com>
2026-04-04 15:24:15 +0530

    1//

--- src/mini_diffusion/diffusion.py
+++ src/mini_diffusion/diffusion.py
@@ -10,14 +10,22 @@ class Diffusion(nn.Module):
         self.config = config.diffusion
         self.device = torch.device(device)
         
-        T = self.config.timesteps
+         
+        s = 0.008
+        tbyT = torch.arange(
+            start=0, end=self.config.timesteps, step=1, device=device
+        ) / self.config.timesteps
+        
+        t = ((tbyT + s) / (1 + s)) * (torch.pi / 2)
+        
+        alpha_hat = (torch.cos(t) ** 2)
+        
+        alpha_hat = alpha_hat / alpha_hat[0]
 
-        beta = torch.linspace(1e-4, 0.02, T, device=device)
-
-        alpha = 1.0 - beta
-
-        alpha_hat = torch.cumprod(alpha, dim=0)
-
+        alpha = torch.ones_like(alpha_hat)
+        alpha[1:] = alpha_hat[1:] / alpha_hat[:-1]
+        beta = 1 - alpha
+        
         self.register_buffer("beta", beta)
         self.register_buffer("alpha", alpha)
         self.register_buffer("alpha_hat", alpha_hat)
